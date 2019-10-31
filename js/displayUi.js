31b2bfdc1eca77e3621f74d8ad4a4276cba65a23
DrewMakes <hey@drewmakes.com>
2019-10-31 16:57:54 -0400

    size images based on div size

--- js/displayUi.js
+++ js/displayUi.js
@@ -5,17 +5,42 @@ function displayUi(imgList) {
     document.getElementsByClassName(div)[0].innerHTML = "";
   });
 
-  console.log("IMAGES: ", imgList);
+  // console.log("IMAGES: ", imgList);
   for (let [index, img] of imgList.entries()) {
     show_image(index, img.src, img.width, img.height);
   }
 }
+var getImgSize = function(divWidth, divHeight, imgWidth, imgHeight) {
+  var imgAspectRatio = imgWidth / imgHeight;
+  if (imgWidth > divWidth) {
+    var imgWidth = divWidth;
+    var imgHeight = divWidth / imgAspectRatio;
+  }
+  if (imgHeight > divHeight) {
+    var imgHeight = divHeight;
+    var imgWidth = divHeight * imgAspectRatio;
+  }
+  return { imgWidth, imgHeight };
+};
 function show_image(index, src, width, height) {
   var img = document.createElement("img");
   img.src = src;
   img.width = width;
   img.height = height;
   img.alt = src;
+
+  var divWidth = document.getElementsByClassName(imgDivs[index])[0].clientWidth;
+  var divHeight = document.getElementsByClassName(imgDivs[index])[0]
+    .clientHeight;
+
+  var imgSize = getImgSize(divWidth, divHeight, img.width, img.height);
+  console.log("old img", { width, height });
+  console.log("new img", imgSize);
+  console.log("div", divWidth, divHeight);
+
+  // set img pixel size
+  img.style.width = "" + imgSize.imgWidth + "px";
+  img.style.height = "" + imgSize.imgHeight + "px";
 
   document.getElementsByClassName(imgDivs[index])[0].appendChild(img);
 }
