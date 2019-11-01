284e1bd308f91ec0e356f35d0c87889730305199
DrewMakes <hey@drewmakes.com>
2019-11-01 17:01:54 -0400

    remove calc margin, use flexbox to align imgs

--- js/displayUi.js
+++ js/displayUi.js
@@ -11,7 +11,6 @@ function displayUi(imgList) {
         area: imgSize.imgWidth * imgSize.imgHeight,
         imgWidth: imgSize.imgWidth,
         imgHeight: imgSize.imgHeight,
-        widthMargin: imgSize.widthMargin,
         div: div
       });
     });
@@ -35,16 +34,7 @@ function displayUi(imgList) {
     // img size to display
     var newWidth = biggestImgLocation.imgWidth;
     var newHeight = biggestImgLocation.imgHeight;
-    var widthMargin = biggestImgLocation.widthMargin;
-    show_image(
-      div,
-      img.src,
-      img.width,
-      img.height,
-      newWidth,
-      newHeight,
-      widthMargin
-    );
+    show_image(div, img.src, img.width, img.height, newWidth, newHeight);
   }
 }
 var getImgSize = function(divWidth, divHeight, imgWidth, imgHeight) {
@@ -57,11 +47,10 @@ var getImgSize = function(divWidth, divHeight, imgWidth, imgHeight) {
     var imgHeight = divHeight;
     var imgWidth = divHeight * imgAspectRatio;
   }
-  var widthMargin = divWidth - imgWidth;
-  return { imgWidth, imgHeight, widthMargin };
+  return { imgWidth, imgHeight };
 };
 
-function show_image(div, src, width, height, newWidth, newHeight, widthMargin) {
+function show_image(div, src, width, height, newWidth, newHeight) {
   var img = document.createElement("img");
   img.src = src;
   img.width = width;
@@ -71,15 +60,6 @@ function show_image(div, src, width, height, newWidth, newHeight, widthMargin) {
   // set img pixel size
   img.style.width = "" + newWidth + "px";
   img.style.height = "" + newHeight + "px";
-  // center ".b" and ".c" div imgs
-  if (div === "b" || div === "c") {
-    img.style.margin = "0 " + widthMargin / 2 + "px";
-  }
-  // top right img anchor for ".a" div
-  if (div === "a") {
-    console.log("got here", div);
-    img.style.marginLeft = "" + widthMargin + "px";
-  }
 
   document.getElementsByClassName(div)[0].appendChild(img);
 }
