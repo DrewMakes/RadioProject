c655325c59c1805f6c8421c8b3a7999a73ee3066
DrewMakes <hey@drewmakes.com>
2019-11-13 17:15:52 -0500

    fly in from further

--- js/displayUi.js
+++ js/displayUi.js
@@ -89,7 +89,6 @@ function show_image(div, src, width, height, newWidth, newHeight) {
   // set img pixel size
   img.style.width = "" + newWidth + "px";
   img.style.height = "" + newHeight + "px";
-  // img.className += "image";
   img.style.setProperty(
     "--rotation-degree",
     randomIntFromInterval(-80, 80) + "deg"
